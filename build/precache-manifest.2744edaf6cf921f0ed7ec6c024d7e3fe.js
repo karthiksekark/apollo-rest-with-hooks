4a988592cbe24b3e2b9e2c7c04b0897762d1bf29
karthiksekark <karthiksekark@gmail.com>
2020-04-19 14:36:29 +0530

    express added

---
+++ build/precache-manifest.2744edaf6cf921f0ed7ec6c024d7e3fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cd6548f6b786424e65306f832bd16fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cb5421cefb13f442103",
+    "url": "/static/css/main.5e3647ae.chunk.css"
+  },
+  {
+    "revision": "295918b093a319d0aeb0",
+    "url": "/static/js/2.515fdb27.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.515fdb27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cb5421cefb13f442103",
+    "url": "/static/js/main.174a0659.chunk.js"
+  },
+  {
+    "revision": "512fb3118c2c241a9388",
+    "url": "/static/js/runtime-main.1aa73ed7.js"
+  }
+]);
